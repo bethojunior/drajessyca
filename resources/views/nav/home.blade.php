f6d8f4965650a23efaa90ba154c915b0c7da14eb
Betho Jr <hgomescostajr@gmail.com>
2020-10-19 20:52:59 -0300

    layout

--- resources/views/nav/home.blade.php
+++ resources/views/nav/home.blade.php
@@ -12,7 +12,7 @@
     <div class="col l3 s12">&nbsp</div>
     <div class="row col l7">
         <div class="col l4 s12">
-            <img class="responsive-img" src="{{ asset('assets/images/dra1.jpg') }}">
+            <img class="responsive-img" src="{{ asset('assets/images/dra.png') }}">
         </div>
         <div class="col l8 s12">
             <span class="isMobileCenter" style="font-size: 4vh; font-family: 'Raleway'">Dra Jessyca Martins</span><br>
@@ -32,55 +32,71 @@
     <div class="center">
         <span><h4 style="color: white">Serviços</h4></span>
     </div>
-    <div class="col l3"></div>
+    <div class="col l2"></div>
     <div class="col l9 s12">
-        <div class="col s12 l2">
+        <div class="col s12 l3">
             <div class="card">
                 <div class="card-image">
                     <img src="http://dratallitadermatologista.com.br/images/patologias-da-pele-1.jpg">
-                    <span class="card-title">Card Title</span>
+                    <span class="card-title">Toxina botulina</span>
                 </div>
-                <div class="card-body">
-
+                <div class="card-body center">
+                    <span></span>
                 </div>
             </div>
-        </div>
-        <div class="col s12 l2">
-            <div class="card">
-                <div class="card-image">
-                    <img src="http://dratallitadermatologista.com.br/images/patologias-da-pele-1.jpg">
-                    <span class="card-title">Card Title</span>
-                </div>
-                <div class="card-body">
-
-                </div>
+            <div class="divider"></div>
+            <div class="white-text isMobileCenter">
+                Toxina botulínica, o famoso Botox®, se origina de uma bactéria chamada Clostridium botulinum. Essa toxina é utilizada para prevenir ou amenizar rugas e linhas de expressão na região dos olhos -como pés de galinha-, região da testa ou no espaço entre as sobrancelhas
             </div>
         </div>
-        <div class="col s12 l2">
+        <div class="col s12 l3">
             <div class="card">
                 <div class="card-image">
                     <img src="http://dratallitadermatologista.com.br/images/patologias-da-pele-1.jpg">
-                    <span class="card-title">Card Title</span>
+                    <span class="card-title">Preenchimentos</span>
                 </div>
-                <div class="card-body">
-
+                <div class="card-body center">
+                    <span></span>
                 </div>
             </div>
+            <div class="divider"></div>
+            <div class="white-text isMobileCenter">
+                Preenchimento cutâneo é uma técnica empregada para correção de sulcos, rugas e cicatrizes, por meio da injeção de substâncias sob a área da pele a ser tratada, elevando-a e, assim, diminuindo sua profundidade. Um dos principais objetivos dos preenchedores é repor o volume da face, melhorar seus contornos e formas. O ácido hialurônico é o principal preenchedor utilizado hoje.
+            </div>
         </div>
-        <div class="col s12 l2">
+        <div class="col s12 l3">
             <div class="card">
                 <div class="card-image">
                     <img src="http://dratallitadermatologista.com.br/images/patologias-da-pele-1.jpg">
-                    <span class="card-title">Card Title</span>
+                    <span class="card-title">Bioestimuladores de colágeno</span>
                 </div>
-                <div class="card-body">
-
+                <div class="card-body center">
+                    <span class=""></span>
                 </div>
+            </div>
+            <div class="divider"></div>
+            <div class="white-text isMobileCenter">
+                Bioestimuladores são substâncias que estimulam a produção de colágeno quando injetadas em determinadas camadas da pele. Podem ser utilizados para reposição de volume a longo prazo e diminuição da flacidez.
+            </div>
+        </div>
+        <div class="col s12 l3">
+            <div class="card">
+                <div class="card-image">
+                    <img src="http://dratallitadermatologista.com.br/images/patologias-da-pele-1.jpg">
+                    <span class="card-title">Fios</span>
+                </div>
+                <div class="card-body center">
+                    <span></span>
+                </div>
+            </div>
+            <div class="divider"></div>
+            <div class="white-text isMobileCenter">
+                O Lifting com fios de PDO é um novo método de suspensão dos tecidos da face e corpo, com o uso de fios 100% absorvíveis <strong>compostos de polidioxanona (PDO)</strong>  que prometem além lifting dos tecidos o estímulo da produção de colágeno, através do processo cicatricial em torno deles.
             </div>
         </div>
 
     </div>
-    <div class="col l3"></div>
+    <div class="col l1"></div>
 </div>
 
 <script src="{{ asset('js/modules/home/init.js') }}"></script>

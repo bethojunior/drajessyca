e19165eef3274ebe8f47c6a2c6e44c0f60c03bac
Betho Jr <hgomescostajr@gmail.com>
2020-10-19 19:21:25 -0300

    options menu

--- resources/views/nav/home.blade.php
+++ resources/views/nav/home.blade.php
@@ -1,8 +1,31 @@
 <link rel="stylesheet" href="{{ asset('css/nav/default.css') }}">
+<link rel="stylesheet" href="{{ asset('css/home/home.css') }}">
+
 <div class="col l12 s12 carousel carousel-slider">
     <a class="carousel-item" href="#one!"><img src="http://dratallitadermatologista.com.br/images/dra-tallita-home-2.jpg"></a>
     <a class="carousel-item" href="#two!"><img src="http://dratallitadermatologista.com.br/images/dra-tallita-home-2.jpg"></a>
     <a class="carousel-item" href="#three!"><img src="http://dratallitadermatologista.com.br/images/dra-tallita-home-2.jpg"></a>
     <a class="carousel-item" href="#four!"><img src="http://dratallitadermatologista.com.br/images/dra-tallita-home-2.jpg"></a>
 </div>
+
+<div class="middle">
+    <div class="col l3">&nbsp;</div>
+    <div class="row col l7">
+        <div class="col l4 s12">
+            <img class="col l12 s12" src="{{ asset('assets/images/dra1.jpg') }}">
+        </div>
+        <div class="col l8 s12">
+            <span class="" style="font-size: 4vh; font-family: 'Raleway'">Dra Jessyca Martins</span><br>
+            <span>
+                Estética avançada | Dermatologia | Nutrologia
+            </span>
+            <hr>
+            <span class="default-color">
+                Esse site tem o objetivo de fornecer informações sobre os cuidados diários com a pele e esclarecer dúvidas sobre os principais tratamentos disponíveis atualmente na medicina estética no Brasil e no mundo. Boa leitura!
+            </span>
+        </div>
+    </div>
+    <div class="col l3">&nbsp;</div>
+</div>
+
 <script src="{{ asset('js/modules/home/init.js') }}"></script>

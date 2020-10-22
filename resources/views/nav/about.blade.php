f7787367510853beec153b45efea623f69221c06
Betho Jr <hgomescostajr@gmail.com>
2020-10-22 14:56:48 -0300

    correção footer

--- resources/views/nav/about.blade.php
+++ resources/views/nav/about.blade.php
@@ -2,7 +2,7 @@
 <link rel="stylesheet" href="{{ asset('css/about/default.css') }}">
 <div class="row col l12 s12 about">
     <div class="col l2"></div>
-    <div class="row col l10 container">
+    <div class="row col l10 ">
         <div class="col l3 s12">
             <img class="responsive-img col l12 s12" src="{{ asset('assets/images/dr3.jpg') }}">
         </div>
